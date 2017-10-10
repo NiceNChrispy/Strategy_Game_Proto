32e2c0039443764f03460fae847024dee404bf1a
David Greedy <DavidJamesGreedy@gmail.com>
2017-10-10 22:09:58 +0100

    A* Pathfinding
    Added A* pathfinding and replaced Node with PathNode. TODO: Find the
    cause of the weird pathing.

---
+++ Assets/Scripts/Level/PathNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : IHeapItem<PathNode>
+{
+    public Vector3 Position;
+    public float GCost;
+    public float HCost;
+    public bool IsTraversible;
+
+    public float FCost { get { return GCost + HCost; } }
+    public int HeapIndex
+    {
+        get;
+        set;        
+    }
+
+    public PathNode Parent;
+
+    public int CompareTo(PathNode other)
+    {
+        int compare = FCost.CompareTo(other.FCost);
+        if (compare == 0)
+        {
+            compare = HCost.CompareTo(other.HCost);
+        }
+        return -compare;
+    }
+}
