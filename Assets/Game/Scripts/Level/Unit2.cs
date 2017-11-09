83c485f88e9151ec9c9ac9824f87511b57df009d
David Greedy <DavidJamesGreedy@gmail.com>
2017-11-09 11:59:04 +0000

    Unit Controlling

--- Assets/Game/Scripts/Level/Unit2.cs
+++ Assets/Game/Scripts/Level/Unit2.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Unit2 : MonoBehaviour
 {
     [SerializeField] private Agent m_Agent;
+    [SerializeField] private LineRenderer m_LineRenderer;
 
     public Agent Agent
     {
@@ -14,13 +15,71 @@ public class Unit2 : MonoBehaviour
         }
     }
 
+    public Unit2 TargetUnit;
+
     public void Select()
     {
         Game_Manager.Instance.SelectUnit(this);
     }
 
-    public void MoveTo(AStarNode targetNavNode, Action callback)
+    public void SetTarget(Unit2 target)
     {
-        StartCoroutine(m_Agent.PathTo(targetNavNode, callback));
+        TargetUnit = target;
+    }
+    
+    public void ClearTarget()
+    {
+        TargetUnit = null;
+    }
+
+    private void Update()
+    {
+        m_LineRenderer.positionCount = 0;
+        if(TargetUnit != null)
+        {
+            DrawUnitTarget();
+        }
+        if(Agent.HasPath)
+        {
+            DrawAgentPath();
+        }
+    }
+
+    public void DrawUnitTarget()
+    {
+        int pointCount = 40;
+        m_LineRenderer.positionCount = pointCount;
+        float step = 1.0f / (pointCount - 1);
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            float t = i * step;
+            float distance = Vector3.Distance(transform.position, TargetUnit.transform.position);
+            m_LineRenderer.SetPosition(i, Vector3.Lerp(transform.position, TargetUnit.transform.position, t) +
+                                        (Vector3.Lerp(Vector3.up * 0.5f, Vector3.up * Mathf.Sqrt(distance) * 0.5f, Mathf.Sin(Mathf.LerpAngle(0, Mathf.PI, t)))));                                 
+        }
+    }
+
+    public void DrawAgentPath()
+    {
+        int startIndex = 0;
+
+        for (int i = 0; i < m_Agent.Path.Count; i++)
+        {
+            if(m_Agent.Path[i] == m_Agent.ActiveNode)
+            {
+                startIndex = i + 1;
+                break;
+            }
+        }
+
+        m_LineRenderer.positionCount = m_Agent.Path.Count - startIndex + 1;
+
+        m_LineRenderer.SetPosition(0, transform.position + new Vector3(0, 0.01f, 0));
+
+        for (int i = startIndex; i < m_Agent.Path.Count; i++)
+        {
+            m_LineRenderer.SetPosition(i - startIndex + 1, m_Agent.Path[i].Data.Position + new Vector3(0, 0.01f, 0));
+        }
     }
 }
---
+++ Assets/Game/Scripts/Selection/Unit_Controller.cs
@@ -0,0 +1,81 @@
+﻿using Navigation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Unit_Controller : MonoBehaviour
+{
+    [SerializeField] private Interactor m_Interactor;
+    [SerializeField] private Text m_CurrentInteractableText;
+
+    [SerializeField] private Unit2 m_SelectedUnit;
+    [SerializeField] private Unit2 m_TargetUnit;
+    [SerializeField] private Unit2 m_PreviousTargetUnit;
+    [SerializeField] private HexTile m_TargetTile;
+    [SerializeField] private HexTile m_PreviousTargetTile;
+
+    [SerializeField] private Map m_NavMap;
+
+    void Update()
+    {
+        m_CurrentInteractableText.text = m_Interactor.Current ? m_Interactor.Current.gameObject.name : "NONE";
+
+        if(m_Interactor.Current)
+        {
+            m_TargetUnit = m_Interactor.Current.GetComponent<Unit2>();
+            m_TargetTile = m_Interactor.Current.GetComponent<HexTile>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (m_TargetUnit)
+            {
+                if (m_SelectedUnit != m_TargetUnit)
+                {
+                    if (m_SelectedUnit)
+                    {
+                        m_SelectedUnit.ClearTarget();
+                        m_SelectedUnit.Agent.ClearDestination();
+                    }
+                    m_SelectedUnit = m_TargetUnit;
+                }
+            }
+        }
+
+        if (m_SelectedUnit)
+        {
+            if (m_TargetTile && m_TargetTile != m_PreviousTargetTile)
+            {
+                m_SelectedUnit.ClearTarget();
+                if(!m_SelectedUnit.Agent.IsPathing)
+                {
+                    m_SelectedUnit.Agent.SetDestination(m_NavMap[m_TargetTile.X, m_TargetTile.Y]);
+                }
+            }
+            if(m_TargetUnit && m_TargetUnit != m_PreviousTargetUnit)
+            {
+                m_SelectedUnit.Agent.ClearDestination();
+                m_SelectedUnit.SetTarget(m_TargetUnit);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1) && m_SelectedUnit)
+        {
+            if(m_TargetTile)
+            {
+                if (!m_SelectedUnit.Agent.IsPathing)
+                {
+                    m_SelectedUnit.Agent.BeginPathing();
+                }
+            }
+            else if(m_TargetUnit)
+            {
+                Debug.Log("ATTACK");
+            }
+        }
+
+        m_PreviousTargetUnit = m_TargetUnit;
+        m_PreviousTargetTile = m_TargetTile;
+    }
+}
