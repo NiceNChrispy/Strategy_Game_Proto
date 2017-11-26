bf33a45b566516e5a4deb7f9369527165aaf4c61
SilentSupernova <chrisyoules93@gmail.com>
2017-11-26 20:49:04 +0000

    Squad Scriptable

---
+++ Assets/Game/Scripts/Units/Squad.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Squad", menuName = "Squad/ Create New Squad")]
+public class Squad : ScriptableObject {
+
+    public string[] squadNumbers;
+}
