1f0ee185c5bb6a6fe37eca731cff568c66a3dea4
SilentSupernova <chrisyoules93@gmail.com>
2017-11-26 21:10:32 +0000

    Squad stuff now in menu scene

--- Assets/Game/Scripts/Units/Squad.cs
+++ Assets/Game/Scripts/Units/Squad.cs
@@ -1,9 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Squad", menuName = "Squad/ Create New Squad")]
-public class Squad : ScriptableObject {
-
-    public string[] squadNumbers;
-}
+﻿
